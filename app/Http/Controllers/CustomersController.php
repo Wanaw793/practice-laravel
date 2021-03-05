b4d09f448fb1adeee9a9fab49e0ac8523e627e58
Wanaw793 <wanatsushi@outlook.jp>
2021-03-05 14:47:42 +0900

    test

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -26,25 +26,25 @@ class CustomersController extends Controller
     //新規登録画面の表示
     public function create()
     {
-
+        return view ('create');
     }
 
     //詳細画面の表示
     public function detail ()
     {
-
+        return view ('detail');
     }
 
     //編集画面の表示
     public function edit ()
     {
-
+        return view ('edit');
     }
 
     //新規登録画面で登録
     public function store ()
     {
-
+        return view
     }
 
     //編集画面で更新
