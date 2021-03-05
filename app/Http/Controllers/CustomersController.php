b08be2f5d87c80c60e0133a0e609b2b37e426f6d
Wanaw793 <wanatsushi@outlook.jp>
2021-03-05 13:33:45 +0900

    test

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -6,8 +6,18 @@ use Illuminate\Http\Request;
 
 class CustomersController extends Controller
 {
+    //一覧画面
     public function getIndex() {
 
         return view('index');
+    }
+
+    //検索
+    public function search(Request $request)
+    {
+        $last_kana = $request->input('last_kana');
+        $first_kana = $request->input('first_kana');
+        $gender = $request->input('gender');
+        $pref_id = $request->input('pref_id');
     }
 }
