2736a2357d284593b695ae128eaf53bd1a1473c4
Wanaw793 <wanatsushi@outlook.jp>
2021-05-25 23:25:34 +0900

    Controllerを修正

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -26,7 +26,6 @@ class CustomersController extends Controller
     public function getIndex()
     {
         $customers = Customer::all();
-        $customers = Customer::withTrashed()->get();
         $prefs = Pref::all();
         $inputs = [
             'last_kana' => '',
